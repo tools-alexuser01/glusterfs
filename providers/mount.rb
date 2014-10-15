dce5aec5e1c3ae96a393abcf2f8fee58b8674f6f
Josh Farr <josh@creativemarket.com>
2014-10-15 12:05:27 -0700

    try using original provider.

--- providers/mount.rb
+++ providers/mount.rb
@@ -19,6 +19,7 @@
 
 require 'chef/log'
 require 'chef/mixin/shell_out'
+require 'chef/provider/mount/mount'
 
 include Chef::Mixin::ShellOut
 
@@ -29,7 +30,8 @@ def whyrun_supported?
 end
 
 action :create do
-  if mounted?
+  mount_provider = Chef::Provider::Mount::Mount.new(@new_resource, @run_context)
+  if mount_provider.mounted?
     Chef::Log.info "#{ @new_resource } already mounted - nothing to do."
   else
     converge_by("Mount #{ @new_resource }") do
@@ -67,30 +69,4 @@ def mount_glusterfs_volume
     pass 0
     action [:mount, :enable]
   end
-end
-
-# mounted? check lifted from: https://github.com/opscode/chef/blob/master/lib/chef/provider/mount/mount.rb
-def mounted?
-  mounted = false
-
-  # "mount" outputs the mount points as real paths. Convert
-  # the mount_point of the resource to a real path in case it
-  # contains symlinks in its parents dirs.
-  real_mount_point = if ::File.exist? new_resource.mount_point
-                       ::File.realpath(new_resource.mount_point)
-                     else
-                       new_resource.mount_point
-                     end
-
-  shell_out!('mount').stdout.each_line do |line|
-    case line
-    when /^#{device_mount_regex}\s+on\s+#{Regexp.escape(real_mount_point)}\s/
-      mounted = true
-      Chef::Log.debug("Special device #{device_logstring} mounted as #{real_mount_point}")
-    when /^([\/\w])+\son\s#{Regexp.escape(real_mount_point)}\s+/
-      mounted = false
-      Chef::Log.debug("Special device #{$~[1]} mounted as #{real_mount_point}")
-    end
-  end
-  current_resource.mounted(mounted)
 end
